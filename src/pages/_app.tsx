e8bc17a82223a5531608de8e0d1971f17e72ccd8
Nicolas <nicolas.pechart@epitech.eu>
2025-01-15 10:18:18 +0100

    :art: Removed seasonal theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,6 @@ import { ReactElement, ReactNode} from "react";
 import "@styles/scroller.css"
 import "@styles/rooms.css"
 import Head from "next/head";
-import Snowflake from "@components/Theme/Snowflake";
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -26,9 +25,6 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
         <link rel="manifest" href="/manifest.json"/>
       </Head>
       <ThemeProvider theme={theme}>
-      <div id="snowflakes" style={{position: "absolute", top: 0, left: 0, width: "100%", height: "100%", zIndex: 69420, pointerEvents: "none"}}>
-        {Array.from({length: 42}, (_, i) => <Snowflake key={i} />)}
-      </div>
       <CssBaseline />
         {getLayout(<Component {...pageProps} />)}
       </ThemeProvider>
